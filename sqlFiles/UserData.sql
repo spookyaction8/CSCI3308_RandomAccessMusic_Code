336c89459a3e36c8ca0aee5a0cd3f0589a669d6c
Garrett Hempy <gahe0404@colorado.edu>
2018-12-06 15:54:41 -0700

    Back End Setup Complete

--- sqlFiles/UserData.sql
+++ sqlFiles/UserData.sql
@@ -1,7 +1,18 @@
-CREATE TABLE UserData(
-	UserID INT  PRIMARY KEY 		NOT NULL,
-	UserName			TEXT 		NOT NULL,
-	UserPassword		TEXT		NOT NULL,
-	Verified 			BIT			NOT NULL,
+CREATE TABLE AccountData(
+	UserID 				SERIAL PRIMARY KEY	NOT NULL,
+	UserName			TEXT 	NOT NULL,
+	UserPassword		TEXT	NOT NULL,
+	Verified 			INT,
 	ReviewCount			INT
 );
+
+
+INSERT INTO AccountData(UserName, UserPassword, Verified, ReviewCount) VALUES ('Garrett','gahe0404', 0, 0);
+INSERT INTO AccountData(UserName, UserPassword, Verified, ReviewCount) VALUES ('Mattie','madi6851', 0, 0);
+INSERT INTO AccountData(UserName, UserPassword, Verified, ReviewCount) VALUES ('Sanaa','muot5862', 0, 0);
+INSERT INTO AccountData(UserName, UserPassword, Verified, ReviewCount) VALUES ('Priyanka','prka4960', 0, 0);
+INSERT INTO AccountData(UserName, UserPassword, Verified, ReviewCount) VALUES ('Zack' ,'zate8983', 0, 0);
+INSERT INTO AccountData(UserName, UserPassword, Verified, ReviewCount) VALUES ('Matthew' ,'masi9192', 0, 0);
+
+
+

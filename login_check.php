a219888c5f5f04b0b927ac29dbbf093cf07fe167
spookyaction8 <zachary.teutsch@colorado.edu>
2018-12-09 15:05:20 -0700

    NO ONE LIKES PHP ANYWAY

--- login_check.php
+++ login_check.php
@@ -6,6 +6,35 @@
   </head>
 
   <body>
+  <?php
+
+    
+      $conn = pg_connect(getenv("DATABASE_URL"));
+
+      if(!$conn){
+        echo "An error has occured.";
+        exit;
+      }
+
+      $pwd = $_GET['pwd'];
+      $uname = $_GET['uname'];
+
+      $username = pg_query($conn,"SELECT username FROM accountdata WHERE username=$username");
+      echo $username;
+      $password = pg_query($conn,"SELECT userpassword FROM accountdata WHERE userpassword=$pwd");
+      echo $password;
+      /*if($_GET["uname"] == $username && $_GET["psw"] == $password){
+        print "User: " . $username . "verified.<br>"
+        print "Redirecting..."
+      }
+      else{
+        print "Sorry, the username and password you have entered do not match. Please try again."
+      }*/
+  ?> 
+
+
+
+
   stupid head
   </body>
 </html>

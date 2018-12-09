42b617b4f0087e14ccf52cc0730f48a56cad73f1
spookyaction8 <zachary.teutsch@colorado.edu>
2018-12-09 14:17:07 -0700

    testing with full URL

--- create_page.php
+++ create_page.php
@@ -7,10 +7,8 @@
 <body>
 <br>
 <?php
-$db = parse_url("postgres://knimivntjnnyed:91034a3b107bde1899b64ab5f99f935bd4c9eedfdbb74d05435210fe5b0d170d@ec2-54-197-234-33.compute-1.amazonaws.com:5432/demmvp07u8rdvt");
-$db["path"] = ltrim($db["path"], "/");
 
-$conn = pg_connect(getenv("DATABASE_URL"));
+$conn = pg_connect(getenv("postgres://knimivntjnnyed:91034a3b107bde1899b64ab5f99f935bd4c9eedfdbb74d05435210fe5b0d170d@ec2-54-197-234-33.compute-1.amazonaws.com:5432/demmvp07u8rdvt"));
 
 $result = pg_query($conn, "SELECT * FROM accountdata WHERE ");
 

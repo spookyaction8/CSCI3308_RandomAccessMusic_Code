c1b6f191fdd7dd2e5f89afbf2d174a67ee952ef9
spookyaction8 <zachary.teutsch@colorado.edu>
2018-12-09 14:19:56 -0700

    testing with full URL

--- create_page.php
+++ create_page.php
@@ -8,22 +8,22 @@
 <br>
 <?php
 
-$conn = pg_connect(getenv("postgres://knimivntjnnyed:91034a3b107bde1899b64ab5f99f935bd4c9eedfdbb74d05435210fe5b0d170d@ec2-54-197-234-33.compute-1.amazonaws.com:5432/demmvp07u8rdvt"));
+$conn = pg_connect(getenv("DATABASE_URL"));
 
 if (!$conn) {
   echo "An error occurred.\n";
   exit;
 }
 
-$result = pg_query($conn, "SELECT * FROM accountdata WHERE ");
+$result = pg_query($conn, "SELECT * FROM accountdata");
 
 echo $result;
 echo "<br>";
 
-if (!$result) {
+/*if (!$result) {
   echo "An error occurred.\n";
   exit;
-}
+}*/
 
 while ($row = pg_fetch_row($result)) {
   echo "$row[0]";

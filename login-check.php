91da0cb1dbcb2ef929d30a06bf73401452dd6fc6
spookyaction8 <zachary.teutsch@colorado.edu>
2018-12-09 14:35:59 -0700

    zach's stupid typos

--- login-check.php
+++ login-check.php
@@ -8,8 +8,10 @@
         echo "An error has occured.\n";
         exit;
       }
+
       $pwd = $_GET['pwd'];
-      $username + $_GET['uname']
+      $username + $_GET['uname'];
+
       $username = pg_query($conn,"SELECT username from accountdata WHERE username=$username");
       echo $username;
       $password = pg_query($conn,"SELECT userpassword from accountdata WHERE password=$pwd");

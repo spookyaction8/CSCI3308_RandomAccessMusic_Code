98107df10e1e6d82effc50ed51f4bd5654b8676a
masi9192 <43587213+masi9192@users.noreply.github.com>
2018-12-10 15:30:43 -0700

    reviews! Kind of

--- songPage.php
+++ songPage.php
@@ -85,6 +85,64 @@ $rowReview = pg_fetch_row($resultReview);
 		<h1><b>Average review score: </b><?php echo $rowSong[3]; ?>/5</h1>
 			<br><br><br><br><br><br><br><br><br>
 		<form action="/writeReview.php" class="btn btn-outline-success" style="float:right;">Write a Review</button>
+			<div class="container">
+		    <div class="row">
+		        <div class="col-sm-8">
+		            <div class="panel panel-white post panel-shadow">
+		                <div class="post-heading">
+		                    <div class="pull-left image">
+		                        <img src="http://bootdey.com/img/Content/user_1.jpg" class="img-circle avatar" alt="user profile image">
+		                    </div>
+		                    <div class="pull-left meta">
+		                        <div class="title h5">
+		                            <a href="#"><b>Ryan Haywood</b></a>
+		                            made a post.
+		                        </div>
+		                        <h6 class="text-muted time">1 minute ago</h6>
+		                    </div>
+		                </div>
+		                <div class="post-description">
+		                    <p>Bootdey is a gallery of free snippets resources templates and utilities for bootstrap css hmtl js framework. Codes for developers and web designers</p>
+		                    <div class="stats">
+		                        <a href="#" class="btn btn-default stat-item">
+		                            <i class="fa fa-thumbs-up icon"></i>2
+		                        </a>
+		                        <a href="#" class="btn btn-default stat-item">
+		                            <i class="fa fa-thumbs-down icon"></i>12
+		                        </a>
+		                    </div>
+		                </div>
+		            </div>
+		        </div>
+		        <div class="col-sm-8">
+		            <div class="panel panel-white post panel-shadow">
+		                <div class="post-heading">
+		                    <div class="pull-left image">
+		                        <img src="http://bootdey.com/img/Content/user_1.jpg" class="img-circle avatar" alt="user profile image">
+		                    </div>
+		                    <div class="pull-left meta">
+		                        <div class="title h5">
+		                            <a href="#"><b>Ryan Haywood</b></a>
+		                            made a post.
+		                        </div>
+		                        <h6 class="text-muted time">1 minute ago</h6>
+		                    </div>
+		                </div>
+		                <div class="post-description">
+		                    <p>Bootdey is a gallery of free snippets resources templates and utilities for bootstrap css hmtl js framework. Codes for developers and web designers</p>
+		                    <div class="stats">
+		                        <a href="#" class="btn btn-default stat-item">
+		                            <i class="fa fa-thumbs-up icon"></i>2
+		                        </a>
+		                        <a href="#" class="btn btn-default stat-item">
+		                            <i class="fa fa-thumbs-down icon"></i>12
+		                        </a>
+		                    </div>
+		                </div>
+		            </div>
+		        </div>
+		    </div>
+		</div>
 
 
 

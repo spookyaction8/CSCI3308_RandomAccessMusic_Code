6ed869e74c9fb92cfcf556fe0552f49f45c63eb5
spookyaction8 <zachary.teutsch@colorado.edu>
2018-12-09 20:02:48 -0700

    charts mess around

--- charts.php
+++ charts.php
@@ -40,11 +40,15 @@
     </nav>
 
       <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-1">
+        </div>
+        <div class="col-md-5">
           <h1> Global Charts </h1>
         </div>
-        <div class="col-md-6" >
+        <div class="col-md-5" >
           <h1> United States Charts </h1>
+        </div>
+        <div class="col-md-1">
         </div>
       </div>
       <div class="link" style="overflow: hidden;">

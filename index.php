a245383f0f9b0ac969eff8307961f32982b611e4
Garrett Hempy <gahe0404@colorado.edu>
2018-12-11 16:20:04 -0700

    :monkey:

--- index.php
+++ index.php
@@ -134,7 +134,7 @@ index HTML
           </div>
     <?php
       $conn = pg_connect(getenv("DATABASE_URL"));
-      pg_query("UPDATE Reviews WHERE (UID=1 AND SID=1) SET rating=5");
+      pg_query("UPDATE Reviews WHERE UID=1 SET rating=5");
     ?>
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>

930bed1ab6ce195a5ce8a4c735e264f406a16bc0
spookyaction8 <zachary.teutsch@colorado.edu>
2018-12-12 12:53:30 -0700

    review process

--- songPage.php
+++ songPage.php
@@ -71,7 +71,7 @@ if (pg_num_rows($resultSong)==0) {
 $variableSong = (int)$resultSong[0];
 
 
-$resultReview = pg_query($conn,"SELECT rating, textreview, uid, totalVotes FROM reviews WHERE (SID::INTEGER)=1");
+$resultReview = pg_query($conn,"SELECT rating, textreview, uid, totalVotes FROM reviews WHERE (SID::INTEGER)=$variableSong");
 $rowReview = pg_fetch_row($resultReview);
 if (!$resultReview) {
   echo "An error occurred. Section 2\n";

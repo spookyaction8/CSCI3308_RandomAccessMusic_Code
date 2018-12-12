6c43e764ee68db6fe7c5c3565c9459ebe19f758c
masi9192 <masi9192@colorado.edu>
2018-12-11 17:29:51 -0700

    test

--- index.php
+++ index.php
@@ -135,7 +135,7 @@ index HTML
     <?php
       $conn = pg_connect(getenv("DATABASE_URL"));
       pg_query($conn,"UPDATE Reviews WHERE UID=1 SET rating=5");
-
+			echo "This just ran";
     ?>
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
